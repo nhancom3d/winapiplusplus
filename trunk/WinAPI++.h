28c311e213a82dc988cbcf8c4637f3e847ea8354
crystalultima <crystalultima@015ccc24-66da-0dbf-c72d-a91e2e47c4ff>
2010-10-19 14:45:58 +0000

    Init

---
+++ trunk/WinAPI++.h
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////
+// Copyright 2010 Putta Khunchalee
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef _WINAPIPP_H_
+#define _WINAPIPP_H_
+
+#ifndef __cplusplus
+#error WinAPI++ Must be using in C++ Project
+#endif
+
+#if defined(WINAPIPP_EXPORT)
+#define WINAPIPP_API __declspec(dllexport)
+#elif defined(WINAPIPP_STATICLIB)
+#define WINAPIPP_API
+#else
+#define WINAPIPP_API __declspec(dllimport)
+#endif
+
+namespace WinApiPP
+{
+	// WinAPI++ Error Code
+	enum ERRORCODE
+	{
+		ERRORCODE_SUCCESS,
+		ERRORCODE_INVALID_PARAMETER,
+		ERRORCODE_WINDOWS_ERROR
+	};
+
+	namespace KERNEL32
+	{
+		// Save Windows SDK Macro
+#pragma push_macro("GetLastError")
+
+		// Destroy Windows SDK Macro
+#undef GetLastError
+
+		// Declaration Function
+		WINAPIPP_API ERRORCODE WINAPI GetLastError(std::string& strError, DWORD dwError = ::GetLastError()) throw();
+		WINAPIPP_API ERRORCODE WINAPI GetLastError(std::wstring& strError, DWORD dwError = ::GetLastError()) throw();
+
+		// Restore Windows SDK Macro
+#pragma pop_macro("GetLastError")
+	}
+}
+
+#endif /* WINAPIPP_H_ */
---
+++ trunk/targetver.h
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////
+// Copyright 2010 Putta Khunchalee
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef _TARGET_VER_H_
+#define _TARGET_VER_H_
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <winsdkver.h>
+#define _WIN32_WINNT 0x0502 // Windows XP SP1
+#include <SDKDDKVer.h>
+
+#endif /* _TARGET_VER_H_ */
