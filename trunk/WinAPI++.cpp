8c48da8af1c4d0ffd1584b7a5acd908530ee955c
crystalultima <crystalultima@015ccc24-66da-0dbf-c72d-a91e2e47c4ff>
2011-04-14 14:08:20 +0000

    Update copyright year

--- trunk/WinAPI++.cpp
+++ trunk/WinAPI++.cpp
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////
-// Copyright 2010 Putta Khunchalee
+// Copyright 2010 - 2011 Putta Khunchalee
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
